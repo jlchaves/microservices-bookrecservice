744f4586c158088931a6abd94f01552028ccfebb
Chaves <jose.luis.chaves@accenture.com>
2018-06-08 18:28:04 +0200

    initial configuration

---
+++ src/main/java/deors/demos/microservices/bookrecservice/Book.java
@@ -0,0 +1,49 @@
+package deors.demos.microservices.bookrecservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(Long id, String title, String author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Id @GeneratedValue private Long id;
+	private String title;
+    private String author;
+
+    @Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
+
+}
